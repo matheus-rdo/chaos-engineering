bcf880f63ce2cf2426d3075680af3ce02580fbf4
matheushr97 <matheushr97@gmail.com>
2020-06-05 20:15:11 -0300

    Update currency service

--- scripts/fault/clear.sh
+++ scripts/fault/clear.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
 kubectl apply -f ../../manifests/3-app-manifests.yaml
+kubectl delete -f ./manifests/unstable-currency-service.yaml
 
 echo "> Todas as injeções de falhas foram desativadas"
--- scripts/fault/unstable-currency-service.sh
+++ scripts/fault/unstable-currency-service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)/manifests
 kubectl apply -f ./unstable-currency-service.yaml
-echo "> Solicitada atualização do 'currencyservice', para injetar 20% de chance de erro nas requisições"
+echo "> Solicitada criação do 'currencyservice' na versão 'risky', para injetar 20% de chance de erro nas requisições"
