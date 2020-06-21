640dc836f401e2890d1a34562d22437067099566
matheushr97 <matheushr97@gmail.com>
2020-06-20 23:53:13 -0300

    Add terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  default = "chaos-engineering"
+}
+
+variable "project" {
+  default = "tcc-engenharia-do-caos"
+}
+
+variable "location" {
+  default = "us-central1"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
